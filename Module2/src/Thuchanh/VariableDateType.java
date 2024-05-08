261386f782fd2e36870ac5452e36c4b71d9a7af0
baoky1999 <kinataru0123@gmail.com>
2024-05-08 13:27:41 +0700

    first commit

---
+++ Module2/src/Thuchanh/VariableDateType.java
@@ -0,0 +1,19 @@
+package Thuchanh;
+
+public class VariableDateType {
+    public static void main(String[] args) {
+        int i = 10; //khai báo biến i là kiểu int có gía trị là 10
+        float f = 20.5f; // khai báo biến f là kiểu float có giá trị là 20.5
+        double d = 20.5; //khai báo biến d là kiểu double có giá trị là 20.5
+        boolean b = true; // khai báo biến b là kiểu boolean có giá trị là true
+        char c = 'a'; // khai báo biến c là kiểu char có giá trị là ký tự a
+        String s = "Hà Nội"; //khai báo biến s là kiểu String có giá trị là chuỗi Hà Nội
+
+        System.out.println("i: " + i);
+        System.out.println("f: " + f);
+        System.out.println("d: " + d);
+        System.out.println("b: " + b);
+        System.out.println("c: " + c);
+        System.out.println("s: " + s);
+    }
+}
