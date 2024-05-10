5f8c6c10c5dbc998aed0b6b5453600446d48a0eb
baoky1999 <kinataru0123@gmail.com>
2024-05-10 14:44:31 +0700

    OOP

---
+++ Module2/src/Baitap/OOP/StopWatch.java
@@ -0,0 +1,61 @@
+package Baitap.OOP;
+
+public class StopWatch {
+
+    private long startTime;
+    private long endTime;
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void start() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public void stop() {
+        endTime = System.currentTimeMillis();
+    }
+
+    public long getElapsedTime() {
+        return getEndTime() - getStartTime();
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[100000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100000);
+        }
+
+        StopWatch stopWatch = new StopWatch();
+
+        stopWatch.start();
+
+        selectionSort(array);
+
+        stopWatch.stop();
+
+        System.out.print(stopWatch.getElapsedTime() + " ms");
+    }
+
+    public static void selectionSort(int[] array) {
+        int length = array.length;
+        for (int i = 0; i < length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+}
+
+
