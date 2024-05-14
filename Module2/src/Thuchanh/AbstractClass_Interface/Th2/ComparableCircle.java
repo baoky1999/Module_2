ce1907cb90a9004bc11c1cbc39133c803943e192
baoky1999 <kinataru0123@gmail.com>
2024-05-14 21:11:02 +0700

    abstractandInterface

---
+++ Module2/src/Thuchanh/AbstractClass_Interface/Th2/ComparableCircle.java
@@ -0,0 +1,26 @@
+package Thuchanh.AbstractClass_Interface.Th2;
+
+import Thuchanh.Abstraction.Th1.Circle;
+public class ComparableCircle extends Circle implements Comparable<ComparableCircle>{
+    public ComparableCircle() {
+    }
+
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+
+    public ComparableCircle(double radius, String color, boolean filled) {
+        super(radius, color, filled);
+    }
+
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) {
+            return 1;
+        }else if (getRadius() < o.getRadius()) {
+            return -1;
+        }else {
+            return 0;
+        }
+    }
+}
