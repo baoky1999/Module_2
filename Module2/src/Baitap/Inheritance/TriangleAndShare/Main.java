431c7de7989302f0ce3b1e8273516a901c6be7b3
baoky1999 <kinataru0123@gmail.com>
2024-05-14 16:05:16 +0700

    inheritance

--- Module2/src/Baitap/Inheritance/TriangleAndShare/Main.java
+++ Module2/src/Baitap/Inheritance/TriangleAndShare/Main.java
@@ -15,9 +15,14 @@ public class Main {
         System.out.print("Nhập side3:");
         double side3 = sc.nextDouble();
 
+        Share share = new Share();
+        share.setColor(color);
+        System.out.println(share.toString());
+
         Triangle triangle = new Triangle( color, side1, side2, side3);
         System.out.println(triangle.toString());
         System.out.println("Diện tích tam giác : " + triangle.getArea());
         System.out.println("Chu vi tam giác : " + triangle.getPerimeter());
+
     }
 }
