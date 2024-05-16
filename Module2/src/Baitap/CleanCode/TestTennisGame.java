d03bfa8189058fa3bf74f77a33a2f1bd14de914e
baoky1999 <kinataru0123@gmail.com>
2024-05-16 18:00:02 +0700

    refactoring

---
+++ Module2/src/Baitap/CleanCode/TestTennisGame.java
@@ -0,0 +1,8 @@
+package Baitap.CleanCode;
+
+public class TestTennisGame {
+    public static void main(String[] args) {
+        TennisGame Game = new TennisGame();
+        System.out.println(Game.getScore(2,2));
+    }
+}
