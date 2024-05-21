dcdf620f60c9f84faa291f039ecf75093b105b17
baoky1999 <kinataru0123@gmail.com>
2024-05-21 20:30:15 +0700

    Queue_LinkedList

---
+++ Module2/src/Thuchanh/DSA_Stack_Queue/Queue_LinkedList/Node.java
@@ -0,0 +1,11 @@
+package Thuchanh.DSA_Stack_Queue.Queue_LinkedList;
+
+public class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}
